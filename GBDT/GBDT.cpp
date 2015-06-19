7c5f92fbfdb74c45f1be28b536c84da391c3137d
Administrator <aron@ubuntu.ubuntu-domain>
2015-06-19 23:47:26 +0800

    push

--- GBDT/GBDT.cpp
+++ GBDT/GBDT.cpp
@@ -1,60 +1,56 @@
 
 // gbdt implementation source file
 
-#include "gbdt.h"
+#include "GBDT.h"
+
+namespace suml {
 
 namespace gbdt {
 
 // implementation of the functions of regression
+void GradientBoostingRegressionTree::train(int32_t opt_type) {
 
-template <class T>
-void GradientBoostingRegressionTree<T>::train(std::vector<std::vector<float> > &trainingX,
-		std::vector<T> &trainingY) {
-	
-	if (0 == trainingX.size()) {
+	if (0 == _feature.size()) {
 		std::cerr << "Error Input Training Feature!" << std::endl;
 		return ;
 	}
-	int32_t sampleCnt = trainingY.size();
-	int32_t featureDim = trainingX[0].size();
+	
+	int32_t sampleCnt = _label.size();
+	int32_t featureDim = _feature.size();
+	
 	for (int32_t i = 0; i < sampleCnt; i ++) {
-		this->m_vTempGradient.push_back(0.0);
-		this->m_vGradient.push_back(trainingY[i]);
+		m_vTempGradient.push_back(0.0);
+		m_vGradient.push_back(_label[i]);
 	}
 
-	for (int32_t i = 0; i < this->m_nTreeNum; i ++) {	
-		Tree<T>* subTree = new RegressionTree<T>(this->m_nTreeDepth, this->m_nNodeCnt, this->m_bMultiThreadOn);
-		subTree->setData(trainingX, this->m_vGradient);
+	for (int32_t i = 0; i < m_nTreeNum; i ++) {	
+		
+		suml::tree::RegressionTree* subTree = new suml::tree::RegressionTree(m_nTreeDepth, m_nNodeCnt, m_bMultiThreadOn, true);
+
+		subTree->setData(_feature, m_vGradient);
 
 		subTree->train();
 		
 		for (int32_t j = 0; j < sampleCnt; j ++) {
-			T value = subTree->predict(trainingX[j]);
-			this->m_vTempGradient[j] += value;
-			this->m_vGradient[j] = trainingY[j] - this->m_fLearningRate * this->m_vTempGradient[j];	
+
+			float value = subTree->predict(_feature[j]);
+			m_vTempGradient[j] += value;
+			m_vGradient[j] = _label[j] - m_fLearningRate * m_vTempGradient[j];	
+		
 		}
-		this->trees.push_back(subTree);
+		trees.push_back(subTree);
 	}	
 };
 
-template <class T>
-T GradientBoostingRegressionTree<T>::predict(const std::vector<T> &feature) {
-	T value = 0;
-	for (int j = 0; j < this->m_nTreeNum; j ++) {
-		value += this->trees[j]->predict(feature);
+float GradientBoostingRegressionTree::predict(const std::vector<float> &feature) {
+	float value = 0;
+	for (int j = 0; j < m_nTreeNum; j ++) {
+		value += trees[j]->predict(feature);
 	}
-	return value * this->m_fLearningRate;
+	return value * m_fLearningRate;
 }
 
 
-// implementation of the functions of classification
-
-template <class T>
-void GradientBoostingClassificationTree<T>::train() {
-}
-
-template <class T>
-T GradientBoostingClassificationTree<T>::predict(const std::vector<float> &feature) {
 }
 
 };
--- GBDT/GBDT.h
+++ GBDT/GBDT.h
@@ -5,35 +5,40 @@
 #ifndef __GBDT_H__
 #define __GBDT_H__
 
-#include "tree.h"
-#include "tree.cpp"
 
+#include "Model.h"
+#include "RegTree.h"
 
+namespace suml {
 namespace gbdt{
 
-template <class T>
-
-class GradientBoostingTree {
+class GradientBoostingRegressionTree : public suml::model::Model<float> {
 
 public:
-    std::vector<Tree<T> *> trees;
+    std::vector<suml::tree::RegressionTree *> trees;
     
 	// define for the model
     int32_t m_nTreeNum;                // number of trees
     int32_t m_nTreeDepth;				// max tree node
 	int32_t m_nNodeCnt;				//	 max node count
+	
 	int32_t m_nMinSampleCnt;		// minimum sample count in single node
 
 	float m_fLearningRate;             // learning rate
 
-	std::vector<T> m_vGradient;
-	std::vector<T> m_vTempGradient;
+	std::vector<float> m_vGradient;
+	std::vector<float> m_vTempGradient;
 
 
 	bool m_bMultiThreadOn;
 
-    GradientBoostingTree() {}
-	GradientBoostingTree(int32_t treeNum, int32_t treeDepth, int32_t nodeCnt, int32_t minSampleCnt, float learningRate, bool isMultiThreadOn) :
+    GradientBoostingRegressionTree() {}
+	GradientBoostingRegressionTree(int32_t treeNum, 
+			int32_t treeDepth, 
+			int32_t nodeCnt, 
+			int32_t minSampleCnt, 
+			float learningRate, 
+			bool isMultiThreadOn) :
 		m_nTreeNum(treeNum),
 		m_nTreeDepth(treeDepth),
 		m_nNodeCnt(nodeCnt),
@@ -42,29 +47,16 @@ public:
    		m_bMultiThreadOn(isMultiThreadOn){}
 
 
-    virtual void train(std::vector<std::vector<float> > &trainingX,
-		std::vector<T> &trainingY) {}
-    virtual T predict(const std::vector<T> &feature){}
-};
+    void train(int32_t opt_type);
 
-template <class T>
-class GradientBoostingRegressionTree : public GradientBoostingTree<T> {
-public:
-    GradientBoostingRegressionTree(){}
-	GradientBoostingRegressionTree(int32_t treeNum, int32_t treeDepth, int32_t nodeCnt, int32_t minSampleCnt, float fLearningRate, bool isMultiThreadOn) : GradientBoostingTree<T>(treeNum, treeDepth, nodeCnt, minSampleCnt, fLearningRate, isMultiThreadOn) {}
-	
-	void train(std::vector<std::vector<float> > &trainingX, std::vector<T> &trainingY);
-	T predict(const std::vector<T> &feature);
+    float predict(const std::vector<float> &feature);
+
+	void load_model(const char* model_file_name);
+
+	void dump_model(const char* model_file_name);
 
 };
-
-template <class T>
-class GradientBoostingClassificationTree {
-public:
-    GradientBoostingClassificationTree() {}
-    void train();
-	T predict(const std::vector<float> &feature);
-};
+}
 
 };
 
